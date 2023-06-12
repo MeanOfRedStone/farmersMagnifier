52efb6bead97e49bb6f2da3d41e19cc67f46526a
SeokEui Hong <hongs7701@naver.com>
2023-06-12 18:07:54 +0900

    공유하기 이전 커밋 -메인 페이지 -게시판 링크 추가

---
+++ rootWEB/webApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from webApp import views
+
+urlpatterns = [
+
+    #로그인 후 메인화면
+    path('home/', views.main),
+
+    #게시판 링크
+    #병충해 정보
+    path('information/', views.information),
+    #병충해 판별
+    path('identification/', views.identification),
+    #
+]
---
+++ rootWEB/webApp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+#로그인 후 메인화면
+def main(request):
+    print(">>>>>>debug client path: / main() , render main.html")
+
+    return render(request, 'main.html')
+
+#게시판 링크
+#병충해 정보 링크
+def information(request):
+    print(">>>>>>debug client path: information/ information(), render information.html")
+
+    return render(request, 'information.html')
+
+#병충해 판별 링크
+def identification(request):
+    print(">>>>>>debug client path: identification/ identification(), render identification.html")
+
+    return render(request, 'identification.html')
+
+#
