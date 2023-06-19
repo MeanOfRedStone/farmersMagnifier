9d9990b2deb1aabd8aa0b9dd1ccb4ae932221d27
SeokEui Hong <hongs7701@naver.com>
2023-06-19 18:00:10 +0900

    0619 1. db 연동 : 테이블 user_information 2. 로그인 관련 기능 구현 : 회원가입, 로그인, 세션 유지, try-except, 상태메시지 등등 3. 게시판 기능 일부 구현

--- rootWEB/webApp/urls.py
+++ rootWEB/webApp/urls.py
@@ -4,20 +4,26 @@ from webApp import views
 urlpatterns = [
 
     #로그인 후 메인화면
-    path('home/', views.main),
+    path('home/', views.main, name='home'),
 
     #게시판 링크
     #병충해 정보
     path('information/', views.information),
     #병충해 판별
     path('identification/', views.identification),
+    #정보 공유 게시판
+    path('communicate/', views.communicate),
 
     #회원 기능
-    #로그인
-    path('login/', views.login),
+    #로그인 폼
+    path('login/', views.login, name='login'),
     #가입 폼
-    path('join/', views.join),
+    path('join/', views.join, name='join'),
     #등록
     path('register/', views.register),
+    #로그인
+    path('check/', views.check),
+    #로그아웃
+    path('logout/', views.logout),
 
 ]
--- rootWEB/webApp/views.py
+++ rootWEB/webApp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,12 +10,27 @@ from django.shortcuts import render
 def main(request):
     print(">>>>>>debug client path: / main() , render main.html")
 
+    #로그인 세션 유지
+    if(request.session.get('session_user_id')):
+        print(">>>>>> debug, session exists")
+        context = {}
+        context['name'] = request.session['session_name']
+        context['user_id'] = request.session['session_user_id']
+        return render(request, 'main2.html', context)
+
     return render(request, 'main.html')
 
 #게시판 링크
 #병충해 정보 링크
 def information(request):
     print(">>>>>>debug client path: information/ information(), render information.html")
+    # 로그인 세션 유지
+    if (request.session.get('session_user_id')):
+        print(">>>>>> debug, session exists")
+        context = {}
+        context['name'] = request.session['session_name']
+        context['user_id'] = request.session['session_user_id']
+        return render(request, 'information.html', context)
 
     return render(request, 'information.html')
 
@@ -21,11 +38,33 @@ def information(request):
 def identification(request):
     print(">>>>>>debug client path: identification/ identification(), render identification.html")
 
+    # 로그인 세션 유지
+    if (request.session.get('session_user_id')):
+        print(">>>>>> debug, session exists")
+        context = {}
+        context['name'] = request.session['session_name']
+        context['user_id'] = request.session['session_user_id']
+        return render(request, 'identification.html', context)
+
     return render(request, 'identification.html')
+
+#정보 공유 게시판
+def communicate(request):
+    print(">>>>>>debug client path: communicate/ communicate(), render communicate.html")
+
+    # 로그인 세션 유지
+    if (request.session.get('session_user_id')):
+        print(">>>>>> debug, session exists")
+        context = {}
+        context['name'] = request.session['session_name']
+        context['user_id'] = request.session['session_user_id']
+        return render(request, 'communicate.html', context)
+
+    return render(request, 'communicate.html')
 
 
 #회원 기능
-#로그인
+#로그인 폼
 def login(request):
     # !!!!!!!! 나중에 구현할 것
     # 추후 로그인 시 홈으로 보내는 기능 구현 필요(기타 사이트도 마찬가지이다)
@@ -39,8 +78,70 @@ def join(request):
 
     return render(request, 'join.html')
 
-#등록
-def register(request) :
-    print(">>>>>>debug client path: register/ register(), render login.html")
+#가입 : 정보 db등록
+def register(request):
+    name    = request.POST['name']
+    id      = request.POST['id']
+    pwd     = request.POST['pwd']
+    pwd_con = request.POST['pwd_con']
+
+    print(">>>>>>debug, params : ", name, id, pwd, pwd_con)
+    #비밀번호(pwd)와 비밀번호 확인(pwd_con)이 일치해야 회원가입이 진행됨
+    #그렇지 않은 경우 다시 회원가입 사이트로 돌아간 뒤
+    try:
+        if(pwd == pwd_con):
+            if(user_information.objects.get(user_id=id)):
+                messages.add_message(request, messages.INFO, "이미 존재하는 ID입니다.")
+                return redirect('join')
+            else:
+                user_information(user_name = name, user_id = id, user_password = pwd).save()
+                messages.add_message(request, messages.INFO, "회원가입이 완료되었습니다.")
+                return redirect('login')
+        else:
+            messages.add_message(request, messages.INFO, "비밀번호를 확인하세요.")
+            return redirect('join')
+    #입력하지 않는 정보가 있을 경우 try-except로 처리
+    except user_information.DoesNotExist:
+        user = None
+        messages.add_message(request, messages.INFO, "입력하지 않는 정보가 있습니다.")
+        return redirect('join')
+
+#로그인
+def check(request):
+    id  = request.POST['id']
+    pwd = request.POST['pwd']
+    print('>>>>> debug, params ', id, pwd)
+
+    #id, pwd가 틀렸을 경우 방지하기 위해서 try-except
+    try:
+        user = user_information.objects.get(user_id =id, user_password = pwd)
+    except user_information.DoesNotExist:
+        user = None
+    print(">>>>> debug, result = ", user)
+
+    if(user):
+        # 데이터를 심는 작업  - 콘텍스트 심어줘서 동적인 작업 해주는 것임
+        # dict 형식으로
+        # 세션 생성
+        request.session['session_name'] = user.user_name
+        request.session['session_user_id'] = user.user_id
+        context = {}
+        context['name'] = request.session['session_name']
+        context['user_id'] = request.session['session_user_id']
+        return render(request, 'main2.html', context)
+
+    elif(user == None):
+        messages.add_message(request, messages.INFO, "없는 회원 정보 입니다.")
+        return redirect('login')
+
+def logout(request):
+    print(">>>>>> debug, client path : logout / logout() call, reditrect home")
+    request.session['session_name'] = {}
+    request.session['session_user_id'] = {}
+    return redirect('home')
+
+
+
+
 
 
