7515ce64223ae38f785d5fdc28da92d51934610b
TaeSungKim <ssuzi58@gmail.com>
2023-06-23 14:41:32 +0900

    classfication앱 및 템플릿폴더 위치 변경

---
+++ rootWEB/classficationApp/urls.py
@@ -0,0 +1,8 @@
+# from django.urls import path
+# from classficationApp import views
+#
+# urlpatterns = [
+#
+#     path('/upload', views.upload, name ="upload")
+#
+# ]
--- rootWEB/rootWEB/settings.py
+++ rootWEB/rootWEB/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webApp',
+    'classficationApp'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'rootWEB.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'rootWEB/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
