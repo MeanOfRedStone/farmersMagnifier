8f8189ee12a58fffd239894607cacd548efa626f
SeokEui Hong <hongs7701@naver.com>
2023-06-26 04:16:00 +0900

    main.html main2.html 수정 style.css 수정

--- rootWEB/rootWEB/urls.py
+++ rootWEB/rootWEB/urls.py
@@ -26,5 +26,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webApp.urls')),
-    path('', include('classficationApp.urls'))
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
